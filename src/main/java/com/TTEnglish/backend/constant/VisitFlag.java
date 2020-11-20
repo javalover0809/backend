c04f63e1deffc0846be516bdcfd73028a372597c
javalover0809 <280671026@qq.com>
2020-11-20 14:14:27 +0800

    detel all the back end demo and rebegin write it

--- src/main/java/com/TTEnglish/backend/constant/VisitFlag.java
+++
@@ -1,5 +0,0 @@
-package com.TTEnglish.backend.constant;
-public class VisitFlag {
-    public String all_content = "1";
-    public String profile_content = "2";
-}
--- src/main/java/com/TTEnglish/backend/dao/AssessMapper.java
+++
@@ -1,6 +0,0 @@
-package com.TTEnglish.backend.dao;
-
-public interface AssessMapper {
-   void insertAssessComment(String username ,String assess_id ,String assessed_username ,String is_comment ,String is_agree);
-
-}
